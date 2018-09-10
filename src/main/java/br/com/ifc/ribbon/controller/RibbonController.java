1bdb1b85d17010f970a6490ad0faa6dc9d1b0aca
Thiago Colombo <thiago.colombo@infracommerce.com.br>
2018-09-10 15:58:05 -0300

    -- Teste

--- src/main/java/br/com/ifc/ribbon/controller/RibbonController.java
+++ src/main/java/br/com/ifc/ribbon/controller/RibbonController.java
@@ -2,6 +2,7 @@ package br.com.ifc.ribbon.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,6 +12,7 @@ import br.com.ifc.ribbon.client.PagarMe;
 
 @RestController
 @RequestMapping("/ribbon")
+@RefreshScope
 public class RibbonController {
 
 	@Autowired
