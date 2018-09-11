067023a45410412e27436c37776d46d3a339854c
Thiago Colombo <thiago.colombo@infracommerce.com.br>
2018-09-11 15:02:19 -0300

    --

--- src/main/java/br/com/ifc/ribbon/controller/RibbonController.java
+++ src/main/java/br/com/ifc/ribbon/controller/RibbonController.java
@@ -3,6 +3,7 @@ package br.com.ifc.ribbon.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,13 +19,17 @@ public class RibbonController {
 	@Autowired
 	private PagarMe proxy;
 	
+	@Autowired
+	private Environment environment;
+	
 	@Value("${message:Default}")
     private String message;
 
 	@GetMapping("/parser")
 	public String parser(@RequestParam(name="msg", required=false) String msg) {
 
-		return proxy.parser(msg) + " Ribbon:" + message;
+		int porta = Integer.parseInt(environment.getProperty("local.server.port"));
+		return proxy.parser(msg) + " {Ribbon Porta:" + porta + ", Mensagem: " + message + "}";
 	}
 
 }
