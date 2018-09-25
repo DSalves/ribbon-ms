6e0ad376a1f1cada1a3ff9392b9c53513007c4fe
Thiago Colombo <thiago.colombo@infracommerce.com.br>
2018-09-25 12:15:48 -0300

    --

--- src/main/java/br/com/ifc/ribbon/main/Application.java
+++ src/main/java/br/com/ifc/ribbon/main/Application.java
@@ -3,14 +3,14 @@ package br.com.ifc.ribbon.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.ComponentScan;
 
 
-@SpringBootApplication
 @EnableFeignClients("br.com.ifc.ribbon.client")
-@ComponentScan(basePackages= {"br.com.ifc.ribbon.config", "br.com.ifc.ribbon.controller", "br.com.ifc.ribbon.filter"})
 @EnableDiscoveryClient
+@EnableEurekaClient
+@SpringBootApplication(scanBasePackages={"br.com.ifc.ribbon.config", "br.com.ifc.ribbon.controller", "br.com.ifc.ribbon.filter"})
 public class Application {
 
 	public static void main(String[] args) {
